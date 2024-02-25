a5699ad14b967b04db16fea3149d51421cb22d69
TatevikBelokon <man.tatevic@yandex.ru>
2024-02-25 17:54:19 +0300

    temporary rename files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
+import App from './components/appComponent/appComponent';
 
 const Settings = {
   placesCount: 312,
--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,4 +1,4 @@
-import Header from '../../components/header/header';
+import Header from '../../components/headerComponent/headerComponent';
 
 function LoginPage (): JSX.Element{
   return(
